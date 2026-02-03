10a5e0fe5b72713f75d632885f02490cdc2886ec
Mik8k <brabobs998@gmail.com>
2026-02-03 11:05:44 -0300

    Integrate SQLAlchemy and create Save model
    Added SQLAlchemy integration and Save model for database.

--- portfolio-bra-main/portfolio-bra-main/main.py
+++ portfolio-bra-main/portfolio-bra-main/main.py
@@ -1,10 +1,23 @@
 # Importar
-from flask import Flask, render_template,request, redirect
+from flask import Flask, render_template, request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
 
 
-
+# Inicializando o banco
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portfolio.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
+class Save(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'<Save {self.id}>'
 # Conteúdo da página
 @app.route('/')
 def index():
